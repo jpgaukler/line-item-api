bc60e89af0e79056c1966065816c78df830423c1
Justin Gaukler <jpgaukler@gmail.com>
2026-06-28 12:09:51 -0400

    Update migration.

--- LineItem.Migrations/Migrations/202606251708_ProductSchema/ProductSchema.cs
+++ LineItem.Migrations/Migrations/202606251708_ProductSchema/ProductSchema.cs
@@ -10,7 +10,18 @@ public class ProductSchema : Migration
 {
     public override void Up()
     {
+        Execute.Script(this.GetUpScript("tbl_product_category.sql"));
         Execute.Script(this.GetUpScript("tbl_product.sql"));
+        Execute.Script(this.GetUpScript("tbl_product_version.sql"));
+        Execute.Script(this.GetUpScript("tbl_product_draft.sql"));
+
+        Execute.Script(this.GetUpScript("fn_product_draft_insert.sql"));
+        Execute.Script(this.GetUpScript("fn_product_draft_retrieve_by_id.sql"));
+        Execute.Script(this.GetUpScript("fn_product_draft_update.sql"));
+        Execute.Script(this.GetUpScript("fn_product_draft_delete.sql"));
+        Execute.Script(this.GetUpScript("fn_product_insert.sql"));
+        Execute.Script(this.GetUpScript("fn_product_update_active_version.sql"));
+        Execute.Script(this.GetUpScript("fn_product_version_insert.sql"));
     }
 
     public override void Down()
@@ -19,5 +30,13 @@ public class ProductSchema : Migration
         Execute.Script("DROP TABLE lineitem.product");
         Execute.Script("DROP TABLE lineitem.product_version");
         Execute.Script("DROP TABLE lineitem.product_draft");
+
+        Execute.Script("DROP FUNCTION lineitem.fn_product_draft_insert(BIGINT, INT, JSONB, VARCHAR)");
+        Execute.Script("DROP FUNCTION lineitem.fn_product_draft_retrieve_by_id(BIGINT)");
+        Execute.Script("DROP FUNCTION lineitem.fn_product_draft_update(BIGINT, JSONB, BIGINT)");
+        Execute.Script("DROP FUNCTION lineitem.fn_product_draft_delete(BIGINT)");
+        Execute.Script("DROP FUNCTION lineitem.fn_product_insert(BIGINT, VARCHAR, VARCHAR, BIGINT)");
+        Execute.Script("DROP FUNCTION lineitem.fn_product_update_active_version(BIGINT, INT, BIGINT)");
+        Execute.Script("DROP FUNCTION lineitem.fn_product_version_insert(BIGINT, JSONB, BIGINT)");
     }
 }
